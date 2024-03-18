7c8cb57553f73e9d734d824c93f89a35338ed052
Johnatas michaell <johnatasmichael067@gmail.com>
2024-03-18 16:44:59 -0300

    add_page_category

---
+++ app_meals/lib/Screns/categores_screns.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../compoents/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScrens extends StatelessWidget {
+  const CategoriesScrens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Vamos cozinhar?'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES.map((cat){
+          return CategoryItem(cat);
+          }).toList(),
+      ),
+    );
+  }
+}
